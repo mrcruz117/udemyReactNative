869e499e6a8955542ff95b5ffdd7dd6bf9537f9a
Michael Cruz <mrcruz1989@gmail.com>
2021-07-18 15:32:21 -0600

    added list practice files

--- rn-starter/src/screens/ComponentsScreens.js
+++ rn-starter/src/screens/ComponentsScreens.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const greeting = 'greeting!';
+  const name = 'Michael';
   return (
     <View>
-      <Text style={styles.textStyle}>This is the components screen</Text>
-      <Text>Hi There!!!!</Text>
+      <Text style={styles.textStyle}>Getting started with react native!</Text>
+      <Text style={styles.subHeader}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  subHeader: {
+    fontSize: 20,
   },
 });
 
