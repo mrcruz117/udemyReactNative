e630ffe9fe2662ef9b156471cfa518cae99993a0
Michael Cruz <mrcruz1989@gmail.com>
2021-07-27 16:46:42 -0400

    learned simple rn Button element

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,21 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hello World!</Text>
+      <Button
+      onPress={()=> console.log('button pressed!')}
+      title='Go to Components Demo' />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
