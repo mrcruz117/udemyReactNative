59b620fd9d8a5d9fa88b990c5901770a7dc5684a
Michael Cruz <mrcruz1989@gmail.com>
2021-08-08 10:05:15 -0400

    completed simple counter tracker

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count</Text>
+      <Button
+        title='Increase'
+        onPress={() => {
+          setCounter(counter+1);
+        }}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => {
+          setCounter(counter-1);
+        }}
+      />
+
+      <Text>Current Count: {counter}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({});
 
