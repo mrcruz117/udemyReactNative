a9ffd1cb0e924a1211394f6c791293d80b7aded0
Michael Cruz <mrcruz1989@gmail.com>
2021-08-20 16:25:48 -0400

    completed search bar styling and basic functions/props

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
+
+  // console.log(term)
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term}
+      onTermChange={(newTerm) => setTerm(newTerm)}
+      onTermSubmit={()=> console.log('term submitted!')}
+      />
       <Text>Search Screen</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
