67f5bbb2e64bd73845d9e9d6bbce6b8909b27d62
Michael Cruz <mrcruz1989@gmail.com>
2021-08-20 09:51:22 -0400

    flexbox edits

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,21 +4,24 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>BoxScreen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 2,
+    borderWidth: 3,
     borderColor: 'black',
+    flexDirection: 'row',
+    height: 200,
+    justifyContent: 'center',
   },
   textStyle: {
-    borderWidth: 1,
+    borderWidth: 3,
     borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 20,
   },
 });
 
