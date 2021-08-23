e5f51f00a0f1edc3c797cade5a241679dee54d9f
Michael Cruz <mrcruz1989@gmail.com>
2021-08-23 16:14:17 -0400

    added hook files

---
+++ food/src/components/ResultsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsList = () => {
+  return (
+    <View>
+      <Text>ResultsList</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.creat({});
+export default ResultsList;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,31 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+// import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get('/search', {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: 'Miami',
-        },
-      });
-
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage('Something went wrong');
-    }
-  };
-
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
   return (
     <View>
       <SearchBar
