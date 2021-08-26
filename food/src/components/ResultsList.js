35f9dd6ee2c268127fcb2dc99b0dd481baef2c9b
Michael Cruz <mrcruz1989@gmail.com>
2021-08-26 15:17:36 -0400

    margin styling

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -4,7 +4,7 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
         horizontal
@@ -22,6 +22,11 @@ const styles = StyleSheet.create({
   titleStyle: {
     fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
   },
 });
 export default ResultsList;
