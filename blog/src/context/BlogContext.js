5c73839ee1cce976e53d560eafc561372ee77b64
Michael Cruz <mrcruz1989@gmail.com>
2021-09-07 11:03:35 -0400

    rendered flat list from context

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,7 +3,13 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+  const blogPosts = [
+    { title: 'blog post 1' },
+    { title: 'blog post 2' },
+    { title: 'blog post 3' },
+  ];
+
+  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
