7a150e9aa7394c61fe030bdd8a36592634222903
Michael Cruz <mrcruz1989@gmail.com>
2021-09-09 18:00:13 -0400

    refactor complete. reusable context function complete

--- blog/src/context/createDataContext.js
+++ blog/src/context/createDataContext.js
@@ -3,4 +3,11 @@ import React, { useReducer } from 'react';
 export default (reducer, actions, initialState) => {
   const Context = react.createContext();
 
+  const Provider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return <Context.Provider value={{ state }}>{children}</Context.Provider>;
+  };
+
+  return { Context, Provider };
 };
