df38ef706095e5ab406558ee17cdef5db254dc13
Michael Cruz <mrcruz1989@gmail.com>
2021-09-09 20:10:12 -0400

    fixed bugs. blog posts now deleting through filter function.

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
       return [
         ...state,
@@ -17,12 +19,17 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return () => {
-    dispatch({ type: 'add_blogpost' });
+    dispatch({ type: 'add_blogpost'});
+  };
+};
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id  });
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
