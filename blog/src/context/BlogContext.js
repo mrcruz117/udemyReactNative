aba6fbf2dcaa01f6862ea6b50701f1d62cd4c785
Michael Cruz <mrcruz1989@gmail.com>
2021-09-10 11:18:07 -0400

    New blogs are added but there is an error on the indexScreen button after changing context

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -9,7 +9,8 @@ const blogReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: `Blog Post #${state.length + 1}`,
+          title: action.payload.title,
+          content: action.payload.content,
         },
       ];
     default:
@@ -18,13 +19,13 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost'});
+  return (title, content) => {
+    dispatch({ type: 'add_blogpost', payload: { title, content } });
   };
 };
 const deleteBlogPost = (dispatch) => {
   return (id) => {
-    dispatch({ type: 'delete_blogpost', payload: id  });
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
--- blog/src/screens/CreateScreen.js
+++ blog/src/screens/CreateScreen.js
@@ -5,6 +5,7 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const { addBlogPost } = useContext(Context);
 
   return (
     <View>
@@ -20,7 +21,10 @@ const CreateScreen = () => {
         value={content}
         onChangeText={(text) => setContent(text)}
       />
-      <Button title='Add Blog Post' />
+      <Button
+        onPress={() => addBlogPost(title, content)}
+        title='Add Blog Post'
+      />
     </View>
   );
 };
