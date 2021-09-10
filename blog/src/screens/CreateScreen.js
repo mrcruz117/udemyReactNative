c3e3ef10b296aa124bedb1681cf68978f5309e4d
Michael Cruz <mrcruz1989@gmail.com>
2021-09-10 10:54:20 -0400

    successfully navigating to create screen

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = () => {
+
+  return (
+    <View>
+      <Text>create screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
