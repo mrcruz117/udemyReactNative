d83eb0961c27fefcf1d147a5b0b87d65ea98050a
Michael Cruz <mrcruz1989@gmail.com>
2021-09-11 16:57:22 -0400

    deligated form screens to a default screen to remove repeating code

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
-const EditScreen = () => {
-  return (
-    <View>
-      <Text>Edit Screen</Text>
-    </View>
+const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam('id')
   );
+
+  return <BlogPostForm />;
 };
 
 const styles = StyleSheet.create({});
